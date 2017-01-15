8077a41213386d4400d040162f27dc963cabd13c
shouko <imshouko@gmail.com>
2017-01-15 12:14:15 +0800

    update init.php, auto gen key if not exist

--- server/init.php
+++ server/init.php
@@ -1,18 +1,21 @@
 <?php
+if (!file_exists('config.php')) exit('Config file not found');
+if (file_exists('server.key') && file_exists('server.pub')) exit(0);
+
 require_once 'config.php';
+
 $res = openssl_pkey_new([
   'digest_alg' => SERVER_DIGEST_ALG,
   'private_key_bits' => SERVER_RSA_KEY_SIZE,
   'private_key_type' => OPENSSL_KEYTYPE_RSA,
 ]);
 
-// Get private key
+// get private key
 openssl_pkey_export($res, $privkey, SERVER_KEY_PASS);
 
-// Get public key
-$pubkey=openssl_pkey_get_details($res);
-$pubkey=$pubkey["key"];
+// get public key
+$pubkey = openssl_pkey_get_details($res);
+$pubkey = $pubkey['key'];
 
-var_dump($privkey);
-
-var_dump($pubkey);
+file_put_contents('server.key', $privkey);
+file_put_contents('server.pub', $pubkey);
--- server/server.sh
+++ server/server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+php init.php
 php -S 0.0.0.0:8000 index.php
