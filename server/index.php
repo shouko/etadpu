6e6a185baa6acec88effa9614e8fdfde045380db
shouko <imshouko@gmail.com>
2017-01-15 11:18:30 +0800

    add check for server

---
+++ server/index.php
@@ -0,0 +1,14 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+if (!isset($_GET['func'])) {
+    exit(json_encode([
+        'result' => 'failed'
+    ]));
+}
+
+switch ($_GET['func']) {
+    case 'check':
+        require 'routes/check.php';
+    case 'fetch':
+        require 'routes/fetch.php';
+}
---
+++ server/routes/check.php
@@ -0,0 +1,18 @@
+<?php
+define(UPDATE_DIR, 'updates');
+
+$files = [];
+if (is_dir(UPDATE_DIR)){
+  if ($dh = opendir(UPDATE_DIR)){
+    while (($file = readdir($dh)) !== false){
+      if ($file[0] != '.') $files[] = $file;
+    }
+    closedir($dh);
+  }
+}
+rsort($files);
+
+exit(json_encode([
+  'files' => $files,
+  'ts' => time()
+]));
