601978f5f7d05a402107a35cfc2d7ae31e518c34
shouko <imshouko@gmail.com>
2017-01-15 16:38:27 +0800

    update fetch.php

--- server/routes/fetch.php
+++ server/routes/fetch.php
@@ -3,22 +3,28 @@ if (!isset($_POST['payload'])) {
   throw new Exception('Missing parameters');
 }
 
+$payload = json_decode($_POST['payload']);
+
 $prikey = openssl_pkey_get_private(file_get_contents('server.key'), SERVER_KEY_PASS);
-openssl_private_decrypt(base64_decode($_POST['payload']), $payload, $prikey);
+if (!isset($payload['key'])) throw new Exception('Missing AES secret');
+openssl_private_decrypt(base64_decode($payload['key']), $aes_secret, $prikey);
+$AES = new AESHelper($aes_secret);
 
-$payload = json_decode($payload, 1);
+$content = json_decode($AES->decrypt($payload['content']), 1);
 
-if (!isset($payload['license']) || !checkLicense($payload['license'])) throw new Exception('Invalid licence key');
-if (!isset($payload['file'])) throw new Exception('Missing parameters');
-$filepath = SERVER_UPDATE_DIR.basename(realpath($payload['file']));
+if (!isset($content['license']) || !checkLicense($content['license'])) throw new Exception('Invalid licence key');
+if (!isset($content['file'])) throw new Exception('Missing parameters');
+$filepath = SERVER_UPDATE_DIR.basename(realpath($content['file']));
 if(!file_exists($filepath)) throw new Exception('Invalid update filename');
+if (!isset($content['pubkey'])) throw new Exception('Missing client public key');
 
 $aes_secret = openssl_random_pseudo_bytes(256);
 $AES = new AESHelper($aes_secret);
-openssl_private_encrypt($aes_secret, $aes_secret_crypted, $prikey);
+$pubkey = openssl_pkey_get_public($content['pubkey']);
+openssl_public_encrypt($aes_secret, $aes_secret_crypted, $pubkey);
 
-exit([
+exit(json_encode([
   'key' => base64_encode($aes_secret_crypted),
   'content' => base64_encode($AES->encrypt(file_get_contents($filepath))),
-  'ts' => time()
-]);
+  'ts' => base64_encode($AES->encrypt(time()))
+]));
